541c71e4eebf4eff650a71c0c606a66472cde1b8
BSC  CSC & BCA  FROM AVICHI COLLEGE <avichiminiproject@outlook.com>
2024-10-07 15:14:16 +0000

    flet sample added on mainpy file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Student Management"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+
+if __name__ == '__main__':
+    ft.app(main)
