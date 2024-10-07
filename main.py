650fc62a51827207d49b2e9fd8590ad105ba1519
BSC  CSC & BCA  FROM AVICHI COLLEGE <avichiminiproject@outlook.com>
2024-10-07 22:16:34 +0530

    just played around

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Student Management"
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
-
+    
 if __name__ == '__main__':
     ft.app(main)
